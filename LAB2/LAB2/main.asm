c499dd1bac22814783779733bf19e612dc16fb55
s4m1r4-r <her231298@uvg.edu.gt>
2026-02-12 12:32:12 -0600

    LAB2

--- LAB2/LAB2/main.asm
+++ LAB2/LAB2/main.asm
@@ -1,126 +1,118 @@
+//***************************************************************************
 /*
 * LAB2.asm
 *
-Created: 07/02/2026 19:17:14 
+* Creado: 07/02/2026 11:09:11
 * Autor : Samira Hernández
-* Descripción: 
+* Descripción: Contador de 4 bist (0-F), implementado a un display de 7 segmentos ánodo, utilizando dos botones para incrementar y decrementar. 
 */
-/****************************************/
-// Encabezado (Definición de Registros, Variables y Constantes)
-.include "M328PBDEF.inc"     // Include definitions specific to ATMega328P
+//***************************************************************************
 
+// Encabezado (Definición de Registros, Variables y Constantes)
+.include "M328PBDEF.inc"		// Include definitions specific to ATMega328P
 
 .cseg
-.org 0x0000
+.org 0x00
 
-//Registros 
-.def contador1 = R17
-.def cris = R18
-.def delay1 = R19
-.def delay2 = R20
-.def temp = R21
- /****************************************/
-// Configuración de la pila
-LDI     R16, LOW(RAMEND)
-OUT     SPL, R16
-LDI     R16, HIGH(RAMEND)
-OUT     SPH, R16
+/****************************************/
+;================ STACK =================
+LDI R16, LOW(RAMEND)
+OUT SPL, R16
+LDI R16, HIGH(RAMEND)
+OUT SPH, R16
 /****************************************/
 
-// Configuracion MCU
+;================ TABLA ANODO COMUN =================
+T7S:
+.DB 0x40,0x79,0x24,0x30		//0, 1, 2, 3
+.DB 0x19,0x12,0x02,0x78		//4, 5, 6, 7
+.DB 0x00,0x10,0x08,0x03		//8, 9, A, b
+.DB 0x46,0x21,0x06,0x0E		// C, d, E, F
+
+/****************************************/
+;================ SETUP =================
 SETUP:
-    ;LEDs (A0-A3)-Configurados como salidas
-	LDI R16,0b00001111
-	OUT DDRC, R16
 
-	;Configuración de Botones
-	CBI DDRB, 0
-	CBI DDRB, 1
+; -------- BOTONES PB1 y PB2 --------
+cbi DDRB, 1          // PB1 entrada
+cbi DDRB, 2          // PB2 entrada
+sbi PORTB, 1         // pull-up
+sbi PORTB, 2         // pull-up
 
-	;Activación de Pull-up interno
-	SBI DDRB, 0
-	SBI DDRB, 1
+; -------- DISPLAY --------
+ldi R16, 0b00111111
+out DDRC, R16        // PC0–PC5 salida (a–f)
 
-	;Display 
-	LDI temp, 0xFF
-	OUT DDRD, temp 
+sbi DDRD, 7          // PD7 salida (g)
 
-	CLR contador1
-	CLR cris
-
-	;Configuración de Timer0
-	LDI R16, 0x00
-	OUT TCCR0A, R16
-
-	;Configuración de Prescaler 1024
-	LDI R16, (1<<CS02)|(1<<CS00)
-	OUT TCCR0B, R16
-
-
-
+; -------- VARIABLE --------
+ldi R19, 0           // contador incialmente en 0
+/****************************************/
 
 /****************************************/
-// Loop Infinito
-MAIN_LOOP:
-	
+;================ LOOP =================
+LOOP:
 
-    LDI R16, 0x00
-	OUT TCNT0, R16
+; Cargar valor de tabla
+mov R18, R19		//Copiar contador 
+ldi ZH, HIGH(T7S<<1)	//Apuntar en tabla de memoria
+ldi ZL, LOW(T7S<<1)
+add ZL, R18			//Desplazar según número
+lpm R18, Z			//Leer valor tabla
 
-	;Mostrar el número actual 
-	RCALL MOSTRAR
+; Mostrar a–f en PORTC
+out PORTC, R18
 
-	;Lectura de PINB
-	IN temp, PINB 
+; Mostrar segmento g (bit6)
+sbrc R18, 6		//Bit6 = 1
+sbi PORTD, 7	//Apagar segmento g
 
-	;-----------BOTÓN INCREMENTO---------------
-	SBRS temp, 0
-	RCALL INCREMENTAR
+sbrs R18, 6		//Bit6 = 0
+cbi PORTD, 7	//Encender segmento g
 
-	;---------BOTÓN DECREMENTAR----------------
-	SBRS temp, 1
-	RCALL DECREMENTAR
+; -------- Leer botones --------
+in R16, PINB		//Lee el estado del puerto B
 
-	RJMP MAIN
+; Botón PB2 = 0 (botón presionado) - Incrementa
+sbrs R16, 2
+rjmp DB_INC
+
+; Botón PB1 = 0 (botón presionado) - Decrementar
+sbrs R16, 1
+rjmp DB_DEC
+
+rjmp LOOP
+/****************************************/
 
 /****************************************/
 // NON-Interrupt subroutines
 
-PAUSA: 
-	IN R16, TIFR0		//lee registro de bandera TIMER0
-	SBRS R16, TOV0		//Verifica si hubo overflow
-	RJMP PAUSA			
+;================ ANTIREBOTE + INC =================
 
-	LDI R16, (1<<TOV0)	// Si hubo overflow, limpiar bandera
-	OUT TIFR0, R16
+DB_INC:
+ldi R16, 100	// Retardo
+D1:
+dec R16
+brne D1
 
-	INC cris
-	CPI cris, 6
-	BRNE MAIN_LOOP
+sbis PINB, 2	//Verifica si aún esta presionado
+rjmp DB_INC
 
-	CLR cris
+inc R19		//Incrementa el contador 
+andi R19, 0x0F		//Limitar de 0 a F
+rjmp LOOP
 
-	INC contador1
-	ANDI contador1, 0x0F
+;================ ANTIREBOTE + DEC =================
+DB_DEC:
+ldi R16, 100		//Retardo
+D2:
+dec R16
+brne D2
 
-	MOV R16, contador1
-	OUT PORTC, R16
-	RJMP MAIN_LOOP
+sbis PINB, 1	//Verifica si esta presionado
+rjmp DB_DEC
 
-;DISPLAY
-MOSTRAR: 
-	LDI ZH, HIGH(TABLA<<1)
-	LDI ZL, LOW(TABLA<<1)
-	ADD ZL, contador1
-	LPM temp, Z
-	OUT PORTD, temp 
-	RET
-
-;-----------INCREMENTO--------------
-	RCALL ANTIREBOTE
-	INC CONTADOR
-
-/****************************************/
-// Interrupt routines
-
+dec R19		//Decrementa el contador
+andi R19, 0x0F		//Limita de 0 a F
+rjmp LOOP
 /****************************************/
